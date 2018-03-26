b7976fedf58f9c113b07103823ce108ebaf41405
kingsuk <kingsuk.majumder@gmail.com>
2018-03-27 01:47:31 +0530

    port changed to default

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace PhiOTWeb
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://*:4000")
+                //.UseUrls("http://*:4000")
                 .UseStartup<Startup>()
                 .Build();
     }
