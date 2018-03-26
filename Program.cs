3deaef7da45b270658f080e522c6d8c4dcbb5531
kingsuk <kingsuk.majumder@gmail.com>
2018-03-27 02:27:44 +0530

    minor change to test script

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace PhiOTWeb
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+            
                 .UseUrls("http://*:4000")
                 .UseStartup<Startup>()
                 .Build();
